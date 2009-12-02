855f5dd5e4f285704c8e75376c04f2bca685aafa
caleb.anderson <caleb@circlesoft.net>
2009-12-02 17:14:13 +1300

    was reporting all matches

--- boot/init.rb
+++ boot/init.rb
@@ -31,13 +31,13 @@ require "base/resources.rb"
 IdentityBayes.set_database(SQLITE3_DB);
 
 module Lulz
-   ERROR_LOG=Logger.new "#{LULZ_DIR}/log/error.log"
-   DEBUG_LOG=Logger.new "#{LULZ_DIR}/log/debug.log"
-   ERROR_LOG.level=Logger::DEBUG
-   DEBUG_LOG.level=Logger::DEBUG
-   def self.debug(msg)
-      DEBUG_LOG.info msg
-   end
+	ERROR_LOG=Logger.new "#{LULZ_DIR}/log/error.log"
+	DEBUG_LOG=Logger.new "#{LULZ_DIR}/log/debug.log"
+	ERROR_LOG.level=Logger::DEBUG
+	DEBUG_LOG.level=Logger::DEBUG
+	def self.debug(msg)
+		DEBUG_LOG.info msg
+	end
 
 
 end
@@ -46,19 +46,19 @@ class Object
 end
 
 class URI::HTTP
-   def to_hash
-      return self.to_s.hash
-   end
+	def to_hash
+		return self.to_s.hash
+	end
 end
 
 class URI::Generic
-   def eql?(oth)
-      if oth.class==self.class
-         return self.component_ary.eql?(oth.component_ary)
-      else
-         false
-      end
-   end
+	def eql?(oth)
+		if oth.class==self.class
+			return self.component_ary.eql?(oth.component_ary)
+		else
+			false
+		end
+	end
 end
 # require base datatypes
 Dir.glob("#{LULZ_DIR}/base/datatypes/*.rb").each {|f| require f }
@@ -68,38 +68,39 @@ Lulz::start_db
 Dir.glob("#{LULZ_DIR}/base/agents/*.rb").each {|f| require f }
 b=Lulz::Blackboard.instance
 unless Lulz::USE_CLI
-   p=Lulz::Person.new
-   p.person_id
-   b.user_write(p) 
-   require "web/start.rb"
-   
+	p=Lulz::Person.new
+	p.person_id
+	b.user_write(p) 
+	require "web/start.rb"
+
 else
-   Lulz::CLI::parse_options(b)
-   Lulz::OPTIONS=b.options
-   b.options[:block_until_finished]=true
-   b.run unless b.options[:expanded] or b.options[:run_agent]
-   matches=b.world.matches
-   if b.options[:xml]
+	Lulz::CLI::parse_options(b)
+	Lulz::OPTIONS=b.options
+	b.options[:block_until_finished]=true
+	b.run unless b.options[:expanded] or b.options[:run_agent]
+	matches=b.world.matches
+	if b.options[:xml]
 		Lulz::CLI::to_xml
 		exit
 	end
 	Lulz::CLI::summary(b) if b.options[:summary]
-   b.profiles.clone.sort{|a,b2| a.person_id <=> b2.person_id}.each { |p| 
-	   puts p.to_text; 
-	   next if matches[p].nil? or matches[p]<b.options[:match_threshold]; 
-	   puts 
-   }
-   puts "suggested matches"
-   i=0
-   #b.world.objects.each do |obj|
-   #   puts "#{obj} => #{obj.hash}"
-   #end
-   sorted=b.profiles.clone.sort { |a1,b1| matches[a1].to_f <=> matches[b1].to_f }.reverse
-   sorted.each do |p|
-      next if matches[p].to_f<b.options[:match_threshold]
-      print "#{p.person_id} (#{sprintf('%.3f',matches[p].to_f)}) "
-      i=i+1
-   end
-   Lulz::CLI::ask_matches(b) if b.options[:match_mode]
+	b.profiles.clone.sort{|a,b2| a.person_id <=> b2.person_id}.each { |p| 
+		next if matches[p].nil? or matches[p]<b.options[:match_threshold]; 
+		
+		puts p.to_text; 
+		puts 
+	}
+	puts "suggested matches"
+	i=0
+	#b.world.objects.each do |obj|
+	#   puts "#{obj} => #{obj.hash}"
+	#end
+	sorted=b.profiles.clone.sort { |a1,b1| matches[a1].to_f <=> matches[b1].to_f }.reverse
+	sorted.each do |p|
+		next if matches[p].to_f<b.options[:match_threshold]
+		print "#{p.person_id} (#{sprintf('%.3f',matches[p].to_f)}) "
+		i=i+1
+	end
+	Lulz::CLI::ask_matches(b) if b.options[:match_mode]
 end
 
